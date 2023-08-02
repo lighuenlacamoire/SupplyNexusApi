1d5b528b7c80788cd7256311449a12afb7204b29
Lighuen Lacamoire <lighuenlacamoire@gmail.com>
2023-08-02 16:55:17 -0300

    primer devolucion en prueba

---
+++ lighuenlacamoire-1-onservices/src/Palindromo.API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Palindromo.API.Support.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Palindromo.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(
+            ILogger<ValuesController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet("IsPalindrome")]
+        public Dictionary<string, string> GetIsPalindrome(string value)
+        {
+            var result = new Dictionary<string, string> 
+            {
+                { "IsPalindrome", "No es palíndromo" },
+                { "UTF8", "" },
+                { "ISO-8859-1", "" }
+            };
+
+            if(!string.IsNullOrEmpty(value))
+            {
+                string newValue = value.NormalizeFormat();
+                char[] compare = newValue.ToCharArray();
+                Array.Reverse(compare);
+
+                string reverseValue = new string(compare);
+
+                if(newValue.Equals(reverseValue, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    result["IsPalindrome"] = "Es palíndromo";
+                }
+
+                Encoding iso = Encoding.Latin1;
+                Encoding utf8 = Encoding.UTF8;
+
+                byte[] bytes = Encoding.Default.GetBytes(value);
+
+                result["UTF8"] = utf8.GetString(bytes);
+                result["ISO-8859-1"] = iso.GetString(bytes);
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ lighuenlacamoire-1-onservices/src/Palindromo.API/Support/Helpers/Formatter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Palindromo.API.Support.Helpers
+{
+    public static class Formatter
+    {
+        public static string NormalizeFormat(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return "";
+            var chars = value
+                .ToLower()
+                .Normalize(NormalizationForm.FormD)
+                .ToCharArray()
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                .ToArray();
+
+            return new string(chars);
+        }
+    }
+}
