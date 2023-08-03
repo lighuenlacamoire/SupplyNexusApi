0b370fe1b4a6f9935e207bfed17b2ecdd603274f
Lighuen Lacamoire <lighuenlacamoire@gmail.com>
2023-08-03 15:28:02 -0300

    inicio ejercicio 4

---
+++ lighuenlacamoire-4-onservices/src/MedicalCenter.API/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace MedicalCenter.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        #region Dependencias
+        private readonly ILogger<ValuesController> _logger;
+        #endregion
+
+        #region Constructor
+        public ValuesController(
+            ILogger<ValuesController> logger)
+        {
+            this._logger = logger;
+        }
+        #endregion
+
+        #region Endpoints
+        [HttpGet("CompareHeightAndWeight")]
+        public string GetCompareHeightAndWeight()
+        {
+            return "2-3-5";
+        }
+        #endregion
+    }
+}
